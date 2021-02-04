346d5b87715583bb19f80ef2786183124f1a0867
Christina Cook <cmc.cook7@gmail.com>
2021-02-03 17:09:54 -0700

    Create Card component

---
+++ src/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <p>Image here</p>
+    </div>
+  )
+}
+
+export default Card;
--- src/Movies/Movies.js
+++ src/Movies/Movies.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Card from '../Card/Card';
 import './Movies.css';
 
 const Movies = () => {
   return (
-    <p>Movies go here</p>
+    <section className="movieContainer">
+      <p>Movies go here</p>
+      <Card />
+    </section>
   )
 }
 
