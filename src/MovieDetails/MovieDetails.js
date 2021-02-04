5486b27f4d219ef83a5f9e510208967bf124fb0d
Christina Cook <cmc.cook7@gmail.com>
2021-02-04 15:24:55 -0700

    Style movie display page

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,21 +1,28 @@
-import React from 'react'
+import React from 'react';
+import './MovieDetails.css';
 
 const MovieDetails = ({ currentMovie, backToMain }) => {
   const date = new Date(currentMovie.release_date)
   return (
     <section className="movieDetails">
       <button onClick={backToMain}>Back to Main</button>
-      <h2>{currentMovie.title}</h2>
-      <img
-        src={currentMovie.poster_path}
-        alt={`${currentMovie.title}-poster`}
-      />
-      <img
-        src={currentMovie.backdrop_path}
-        alt={`${currentMovie.title}-poster`}
-      />
-      <p>Average Rating: {currentMovie.average_rating.toFixed(1)}</p>
-      <p>Release Date: {date.toLocaleDateString()}</p>
+      <div className="movieDetailsCard">
+        <img className="poster"
+          src={currentMovie.poster_path}
+          alt={`${currentMovie.title}-poster`}
+        />
+        <span className="movieInfo">
+          <h2>{currentMovie.title}</h2>
+          <p><strong>Average Rating:</strong> {currentMovie.average_rating.toFixed(1)}</p>
+          <p><strong>Release Date:</strong> {date.toLocaleDateString()}</p>
+        </span>
+      </div>
+      <div className="trailerContainer">
+        <img className="trailer"
+          src={currentMovie.backdrop_path}
+          alt={`${currentMovie.title}-poster`}
+        />
+      </div>
     </section>
   );
 }
