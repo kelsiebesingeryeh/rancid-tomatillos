7c6e53be72f7842ac856f3766ae509a4d4c0c428
Christina Cook <cmc.cook7@gmail.com>
2021-02-04 15:02:34 -0700

    Format release date

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,22 +1,23 @@
 import React from 'react'
 
 const MovieDetails = ({ currentMovie, backToMain }) => {
-    return (
-      <section className="movieDetails">
-        <button onClick={backToMain}>Back to Main</button>
-        <h2>{currentMovie.title}</h2>
-        <img
-          src={currentMovie.poster_path}
-          alt={`${currentMovie.title}-poster`}
-        />
-        <img
-          src={currentMovie.backdrop_path}
-          alt={`${currentMovie.title}-poster`}
-        />
-        <p>{currentMovie.average_rating}</p>
-        <p>{currentMovie.release_date}</p>
-      </section>
-    );
+  const date = new Date(currentMovie.release_date)
+  return (
+    <section className="movieDetails">
+      <button onClick={backToMain}>Back to Main</button>
+      <h2>{currentMovie.title}</h2>
+      <img
+        src={currentMovie.poster_path}
+        alt={`${currentMovie.title}-poster`}
+      />
+      <img
+        src={currentMovie.backdrop_path}
+        alt={`${currentMovie.title}-poster`}
+      />
+      <p>Average Rating: {currentMovie.average_rating.toFixed(1)}</p>
+      <p>Release Date: {date.toLocaleDateString()}</p>
+    </section>
+  );
 }
 
 export default MovieDetails
