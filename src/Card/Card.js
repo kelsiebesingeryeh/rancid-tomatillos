7949c5b8254f806210fc3e63ff5b817ecc19bd78
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-06 14:39:40 -0700

    Add movie titles underneath movie cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -5,17 +5,22 @@ import star from '../assets/star.png'
 const Card = ({ title, posterImage, id, avgRating, displayMovieDetails }) => {
   return (
     <div className="movieCard">
-      <img
-        className="moviePosterImage"
-        src={posterImage}
-        alt={`${title} poster`}
-        id={id}
-        onClick={() => displayMovieDetails(id)}
-      />
-      <span className="viewRatings">
-        <img className="star" src={star} alt="star" />
-        <p className="movieRating">{avgRating.toFixed(1)}</p>
-      </span>
+      <div>
+        <img
+          className="moviePosterImage"
+          src={posterImage}
+          alt={`${title} poster`}
+          id={id}
+          onClick={() => displayMovieDetails(id)}
+        />
+        {/* <span className="viewRatings">
+          <img className="star" src={star} alt="star" />
+          <p className="movieRating">{avgRating.toFixed(1)}</p>
+        </span> */}
+      </div>
+      <div className="movieTitles">
+        <p className="movieTitle">{title}</p>
+      </div>
     </div>
   );
 };
