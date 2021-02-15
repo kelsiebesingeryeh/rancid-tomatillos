3d9dbcdf3bffb67d01e7818fd6e7ca1b4ec63e89
Christina Cook <cmc.cook7@gmail.com>
2021-02-15 16:49:10 -0700

    Change single quotes to double for consistency

--- cypress/integration/rancid_tomatillos_spec.js
+++ cypress/integration/rancid_tomatillos_spec.js
@@ -3,7 +3,7 @@ describe("Rancid Tomatillos Home Page", () => {
 
   it("Should see a home button when user visits the homepage", () => {
     cy.visit(baseUrl)
-      .get('img[class="homeIcon"]').should("be.visible")
+      .get("img[class="homeIcon"]").should("be.visible")
   })
 
   it("Should have a main title on the homepage", () => {
@@ -26,7 +26,7 @@ describe("Rancid Tomatillos Home Page", () => {
             )
           })
       cy.visit(baseUrl)
-      .get('div[class="randomMovieImage"]')
+      .get("div[class="randomMovieImage"]")
       .should("be.visible")
   })
 
@@ -44,9 +44,9 @@ describe("Rancid Tomatillos Home Page", () => {
           }
         );
       })
-      .get('h2[class="headerTitle"]')
+      .get("h2[class="headerTitle"]")
       .should("exist")
-      .get('div[class="headerText"]')
+      .get("div[class="headerText"]")
       .should("exist")
   })
 
@@ -64,9 +64,9 @@ describe("Rancid Tomatillos Home Page", () => {
           }
         );
       })
-      .get('img[class="moviePosterImage"]')
+      .get("img[class="moviePosterImage"]")
       .should("be.visible")
-      .get('p[class="movieTitle"]')
+      .get("p[class="movieTitle"]")
       .should("be.visible")
   })
 
@@ -124,35 +124,35 @@ describe("Rancid Tomatillos Home Page", () => {
       .get(".sortDropDown").get("select")
   })
 
-  it('Should default the sort display to all movies', () => {
+  it("Should default the sort display to all movies", () => {
     cy.visit(baseUrl)
       .get(".sortIcon").click()
       .get(".sortDropDown").get("select").select("View All Movies")
       .get(".movieTitle:first").should("contain", "Money Plane")
   })
 
-  it('Should be able to sort movies by rating, high to low', () => {
+  it("Should be able to sort movies by rating, high to low", () => {
     cy.visit(baseUrl)
       .get(".sortIcon").click()
       .get(".sortDropDown").get("select").select("Rating High to Low")
       .get(".movieRating:first").should("contain", "7")
   })
 
-  it('Should be able to sort movies by rating, low to high', () => {
+  it("Should be able to sort movies by rating, low to high", () => {
     cy.visit(baseUrl)
       .get(".sortIcon").click()
       .get(".sortDropDown").get("select").select("Rating Low to High")
       .get(".movieRating:first").should("contain", "3")
   })
 
-  it('Should be able to sort movie titles in alphabetical order', () => {
+  it("Should be able to sort movie titles in alphabetical order", () => {
     cy.visit(baseUrl)
       .get(".sortIcon").click()
       .get(".sortDropDown").get("select").select("Movie Title A to Z")
       .get(".movieTitle:first").should("contain", "2067")
   })
 
-  it('Should be able to sort movie titles in reverse alphabetical order', () => {
+  it("Should be able to sort movie titles in reverse alphabetical order", () => {
     cy.visit(baseUrl)
       .get(".sortIcon").click()
       .get(".sortDropDown").get("select").select("Movie Title Z to A")
@@ -164,23 +164,23 @@ describe("Rancid Tomatillos Home Page", () => {
 describe("Movie Details Page", () => {
   const baseUrl = "http://localhost:3000/movies/694919";
 
-  it.only(`Should display header for a selected movie`, () => {
+  it(`Should display header for a selected movie`, () => {
     cy.visit(baseUrl)
-      .get(".movieDetailsHeader").should('be.visible')
-      .get(".headerTitle").should('contain', "Money Plane")
+      .get(".movieDetailsHeader").should("be.visible")
+      .get(".headerTitle").should("contain", "Money Plane")
   })
 
-  it('Should display a title, image, runtime, and release year in the header', () => {
+  it("Should display a title, image, runtime, and release year in the header", () => {
     cy.visit(baseUrl)
-      .get(".headerTitle").should('contain', 'Money Plane')
+      .get(".headerTitle").should("contain", "Money Plane")
       .get(".headerImage").should("be.visible")
       .get(".runtime").should("contain", "82 minutes")
       .get(".year").should("contain", "2020")
   })
 
-  it('Should display a movie poster', () => {
+  it("Should display a movie poster", () => {
     cy.visit(baseUrl)
-      .get(".poster").should('be.visible')
+      .get(".poster").should("be.visible")
   })
 
   it("Should display a movie overview", () => {
@@ -190,13 +190,13 @@ describe("Movie Details Page", () => {
 
   it("Should display a movie details including release year, runtime, genre, budget, revenue, average rating", () => {
     cy.visit(baseUrl)
-      .get(".movieDetailsList").should("contain", 'Released')
-        .and('contain', 'Runtime')
-        .and('contain', 'Genre')
-        .and('contain', 'Average Rating')
+      .get(".movieDetailsList").should("contain", "Released")
+        .and("contain", "Runtime")
+        .and("contain", "Genre")
+        .and("contain", "Average Rating")
   })
 
-  it('Should be able to click the home icon and display home page', () => {
+  it("Should be able to click the home icon and display home page", () => {
     cy.visit(baseUrl)
       .get(".homeIcon").click()
   })
@@ -216,6 +216,6 @@ describe("Movie Details Page", () => {
 
    it("Should be able to view a movie's trailer", () => {
      cy.visit(baseUrl)
-      .get(".trailerContainer").should('be.visible')
+      .get(".trailerContainer").should("be.visible")
    })
 })
