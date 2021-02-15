294982631a0829bdbc6f71b4407ae6ac55c3d3d1
Christina Cook <cmc.cook7@gmail.com>
2021-02-15 16:03:11 -0700

    Write it statements for search and sort tests

--- cypress/integration/rancid_tomatillos_spec.js
+++ cypress/integration/rancid_tomatillos_spec.js
@@ -4,12 +4,12 @@ describe("Rancid Tomatillos Home Page", () => {
   it("Should see a home button when user visits the homepage", () => {
     cy.visit(baseUrl);
     cy.get('img[class="homeIcon"]').should("be.visible");
-  });
+  })
 
   it("Should have a main title on the homepage", () => {
     cy.visit(baseUrl);
     cy.get("h1").contains("Rancid Tomatillos");
-  });
+  })
 
   it("Should see a random movie image displayed in the header", () => {
     cy.fixture("testMovieData.json")
@@ -28,8 +28,7 @@ describe("Rancid Tomatillos Home Page", () => {
       cy.visit(baseUrl)
       .get('div[class="randomMovieImage"]')
       .should("be.visible");
-  });
-
+  })
 
   it("Should see a movie title and year in the header", () => {
     cy.fixture("testMovieData.json")
@@ -49,7 +48,7 @@ describe("Rancid Tomatillos Home Page", () => {
       .should("exist")
       .get('div[class="headerText"]')
       .should("exist");
-  });
+  })
 
   it("Should see all movies on the homepage", () => {
     cy.fixture("testMovieData.json")
@@ -69,46 +68,71 @@ describe("Rancid Tomatillos Home Page", () => {
       .should("be.visible")
       .get('p[class="movieTitle"]')
       .should("be.visible");
-  });
+  })
 
   it("Should be able to click a movie poster", () => {
     cy.visit(baseUrl);
     cy.get(".movieCard:first").click();
-  });
+  })
 
   it("Should be able to click the home icon and refresh the page", () => {
     cy.visit(baseUrl);
     cy.get(".homeIcon").click();
-  });
+  })
 
   it("Should be able to see an error message if the movies don't display", () => {
-      cy.intercept(
-        "GET",
-        "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
-        {
-          "forceNetworkError": true,
-        }
-      )
-      cy.visit(baseUrl)
-        .get(".errorMessage")
-        .should("contain", "Something went wrong!");
-    })
-    })
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies",
+      {
+        "forceNetworkError": true,
+      }
+    )
+    cy.visit(baseUrl)
+      .get(".errorMessage")
+      .should("contain", "Something went wrong!");
+  })
+
+  it("Should be able to click the search icon and display a search bar", () => {
+
+  })
+
+  it("Should be able to type a movie title into the search bar", () => {
+
+  })
+
+  it("Should see movies that match the user's search input", () => {
+
+  })
+
+  it("Should see a message if no movies match the user's search input", () => {
+
+  })
+
+  it("Should be able to click the sort icon and display a dropdown menu", () => {
+
+  })
+
+  it("Should be able to select an option from the dropdown menu to sort the displayed movies", () => {
+
+  })
+
+})
 
 describe("Movie Details Page", () => {
   const baseUrl = "http://localhost:3000/movies/694919";
-  
+
   it(`Should display header for a selected movie`, () => {
-  cy.visit(baseUrl);
-  cy.get(".movieDetailsHeader").should('be.visible')
+    cy.visit(baseUrl);
+    cy.get(".movieDetailsHeader").should('be.visible')
   })
 
   it('Should display a title, image, runtime, and release year in the header', () => {
-  cy.visit(baseUrl);  
-  cy.get(".headerTitle").should('contain', 'Money Plane')
-  cy.get(".headerImage").should("be.visible");
-  cy.get(".runtime").should("contain", "82 minutes");
-  cy.get(".year").should("contain", "2020");
+    cy.visit(baseUrl);
+    cy.get(".headerTitle").should('contain', 'Money Plane')
+    cy.get(".headerImage").should("be.visible");
+    cy.get(".runtime").should("contain", "82 minutes");
+    cy.get(".year").should("contain", "2020");
   })
 
   it('Should display a movie poster', () => {
@@ -119,34 +143,33 @@ describe("Movie Details Page", () => {
   it("Should display a movie overview", () => {
     cy.visit(baseUrl);
     cy.get(".movieOverview").should("be.visible");
-  });
+  })
 
   it("Should display a movie details including release year, runtime, genre, budget, revenue, average rating", () => {
     cy.visit(baseUrl);
     cy.get(".movieDetailsList").should("contain", 'Released').and('contain', 'Runtime').and('contain', 'Genre').and('contain', 'Average Rating');
-  });
-  
+  })
+
   it('Should be able to click the home icon and display home page', () => {
     cy.visit(baseUrl);
     cy.get(".homeIcon").click()
   })
 
   it("Should be able to see an error message if the movie details don't display", () => {
-      cy.intercept(
-        "GET",
-        "https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919",
-        {
-          "forceNetworkError": true,
-        }
-      )
-      cy.visit(baseUrl)
-        .get(".errorMessage")
-        .should("contain", "Something went wrong!");
+    cy.intercept(
+      "GET",
+      "https://rancid-tomatillos.herokuapp.com/api/v2/movies/694919",
+      {
+        "forceNetworkError": true,
+      }
+    )
+    cy.visit(baseUrl)
+      .get(".errorMessage")
+      .should("contain", "Something went wrong!");
     })
 
-     it("Should be able to view a movie's trailer", () => {
-       cy.visit(baseUrl);
-       cy.get(".trailerContainer").should('be.visible')
-     });
+   it("Should be able to view a movie's trailer", () => {
+     cy.visit(baseUrl);
+     cy.get(".trailerContainer").should('be.visible')
+   })
 })
-
