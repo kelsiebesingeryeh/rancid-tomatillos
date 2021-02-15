ee06cb1e3d935406062702d4c2f8acaf8b9bf557
Kelsie Besinger <kelsiebesinger@gmail.com>
2021-02-15 11:11:51 -0700

    Add functionality to sort movies by dropdown options

--- src/Sort/Sort.js
+++ src/Sort/Sort.js
@@ -2,30 +2,55 @@ import React, { Component } from 'react'
 import './Sort.scss'
 
 class Sort extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            movies: this.props.movies
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: this.props.movies,
+    };
+  }
 
-    sortMoviesByRating = () => {
-        
+  handleChange = (event) => {
+    if (event.target.value === "high") {
+      this.sortMoviesHighToLow('average_rating');
+    } else if (event.target.value === "low") {
+      this.sortMoviesLowToHigh("average_rating");
+    } else if (event.target.value === "alphabetically") {
+        this.sortMoviesLowToHigh('title')
+    } else if (event.target.value === 'reverseAlphabetically') {
+        this.sortMoviesHighToLow('title');
     }
-    
-    render() {
-        return (
-            <form className='sortDropDown'>
-                <label htmlFor="movies">Sort by:</label>
-                <select name="movies" id="movies">
-                    <option value="high">Rating High to Low</option>
-                    <option value="low">Rating Low to High</option>
-                    <option value="alphabetically">Movie Title A to Z</option>
-                    <option value="reverseAlphabetically">Movie Title Z to A</option>
-                </select>
-            </form>
-        )
-    }
+  }
+
+  sortMoviesHighToLow = (key) => {
+    return this.state.movies.sort((a, b) => {
+        if (a[key] > b[key]) {
+            return -1
+        }
+    })
+  }
+
+  sortMoviesLowToHigh = (key) => {
+      return this.state.movies.sort((a, b) => {
+        if (a[key] < b[key]) {
+          return -1;
+        }
+      })
+  }
+
+  render() {
+    return (
+      <form className="sortDropDown">
+        <label htmlFor="movies">Sort by:</label>
+        <select name="movies" id="movies" onChange={this.handleChange}>
+          <option value="placeholder">Choose an option</option>
+          <option value="high">Rating High to Low</option>
+          <option value="low">Rating Low to High</option>
+          <option value="alphabetically">Movie Title A to Z</option>
+          <option value="reverseAlphabetically">Movie Title Z to A</option>
+        </select>
+      </form>
+    );
+  }
 }
 
 export default Sort
